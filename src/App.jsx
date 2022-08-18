e7b2ee356c223fbd656435557f50149980a8f510
Mohamed <mhassankd35@gmail.com>
2022-08-18 17:55:29 -0500

    creates axios GET call for all activity logs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import ActivityFeed from "./components/ActivityFeed.jsx";
-
+import useFetch from "./hooks/useFetch.js";
+import { useState } from "react";
 import Header from "./Header.jsx";
+import axios from "axios";
 
 const App = () => {
+  // const { data, loading, error } = useFetch("/activities");
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const url = "https://aircall-job.herokuapp.com/activities";
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(url)
+      .then((results) => setData(results.data))
+      .catch((error) => console.log(error));
+    setLoading(false);
+  }, []);
+  console.log(data);
   return (
     <div className="container">
       <Header />
       <div className="container-view">
-        <ActivityFeed />
+        {loading ? "loading" : <ActivityFeed data={data} />}
       </div>
     </div>
   );
