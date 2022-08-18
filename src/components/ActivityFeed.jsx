a582f3fafdc6e6b26c88f0a49ad23c8ace6982e8
Mohamed <mhassankd35@gmail.com>
2022-08-18 18:14:24 -0500

    creates call item content on ActivityFeed component

--- src/components/ActivityFeed.jsx
+++ src/components/ActivityFeed.jsx
@@ -1,7 +1,32 @@
 import React from "react";
-
-function ActivityFeed() {
-  return <div>List of Calls</div>;
+import {
+  BsFillTelephoneInboundFill,
+  BsFillTelephoneOutboundFill,
+} from "react-icons/bs";
+function ActivityFeed({ data }) {
+  return (
+    <div className="activity_container">
+      {data.map((call) => (
+        <div className="activity_items" key={call.id}>
+          <div className="activity_icon">
+            {call.direction === "inbound" ? (
+              <BsFillTelephoneInboundFill />
+            ) : (
+              <BsFillTelephoneOutboundFill />
+            )}
+          </div>
+          <div className="activity_caller">{call.from}</div>
+          <div className="activity_caller">{call.from}</div>
+          <div className="activity_called_at">
+            {new Date(call.created_at).toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default ActivityFeed;
