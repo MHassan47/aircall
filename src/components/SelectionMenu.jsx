65049d61bc1d05fdb6f202e07fe257e34046baec
Mohamed <mhassankd35@gmail.com>
2022-08-19 18:39:27 -0500

    refactors local states to be lifted

--- src/components/SelectionMenu.jsx
+++ src/components/SelectionMenu.jsx
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
 import "../css/selectionMenu.css";
-function SelectionMenu(props) {
-  const [isSelected, setIsSelected] = useState("All Calls");
-  console.log(isSelected);
+function SelectionMenu({ archive, setArchive }) {
   return (
     <div className="menu_container">
       <div
         className="all_calls"
-        id={isSelected === "All Calls" ? "selected" : null}
-        onClick={() => setIsSelected("All Calls")}
+        id={!archive ? "selected" : null}
+        onClick={() => {
+          setArchive(false);
+        }}
       >
         All Calls
       </div>
       <div
         className="archived_calls"
-        id={isSelected === "Archived Calls" ? "selected" : null}
-        onClick={() => setIsSelected("Archived Calls")}
+        id={archive ? "selected" : null}
+        onClick={() => {
+          setArchive(true);
+        }}
       >
         Archived Calls
       </div>
