4e184966e14711cbbce94619144b62a7075403aa
Mohamed <mhassankd35@gmail.com>
2022-08-19 18:39:01 -0500

    fixes archiveClickHandler logic

--- src/components/ActivityFeed.jsx
+++ src/components/ActivityFeed.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../css/activityFeed.css";
 import {
   BsFillTelephoneInboundFill,
@@ -6,12 +6,22 @@ import {
 } from "react-icons/bs";
 import { RiArchiveFill, RiInformationFill } from "react-icons/ri";
 import CallInfoModal from "./CallInfoModal.jsx";
+import axios from "axios";
 
-function ActivityFeed({ data }) {
+function ActivityFeed({ data, archive, setIsCallArchived }) {
   const [isOpen, setIsOpen] = useState(false);
   const [callID, setCallID] = useState();
-  const archiveClickHandler = () => {
-    console.log("clicked archive");
+
+  const archiveClickHandler = (callID) => {
+    setIsCallArchived(true);
+
+    axios
+      .post(`https://aircall-job.herokuapp.com/activities/${callID}`, {
+        is_archived: true,
+      })
+
+      .then(() => setIsCallArchived(false))
+      .catch((error) => console.log(error));
   };
   const infoClickHandler = (callID) => {
     setIsOpen(true);
@@ -22,37 +32,43 @@ function ActivityFeed({ data }) {
     setIsOpen(false);
   };
 
-  console.log(isOpen);
-  console.log(callID);
   return (
     <div className="activity_container">
       {isOpen && (
         <CallInfoModal callID={callID} open={isOpen} onClose={onClose} />
       )}
-      {data.map((call) => (
-        <div className="activity_item" key={call.id}>
-          <div className="activity_icon">
-            {call.direction === "inbound" ? (
-              <BsFillTelephoneInboundFill />
-            ) : (
-              <BsFillTelephoneOutboundFill />
-            )}
-            <h1 className="activity_caller">{call.from}</h1>
-          </div>
-          <div className="activity_right">
-            <h2 className="activity_called_at">
-              {new Date(call.created_at).toLocaleTimeString([], {
-                hour: "2-digit",
-                minute: "2-digit",
-              })}
-            </h2>
-            <div className="options_icons">
-              <RiArchiveFill onClick={archiveClickHandler} />
-              <RiInformationFill onClick={() => infoClickHandler(call.id)} />
+
+      {data
+        .filter((item) => item.is_archived === archive)
+        .map((call) => {
+          // if (!call.is_archived)
+          return (
+            <div className="activity_item" key={call.id}>
+              <div className="activity_icon">
+                {call.direction === "inbound" ? (
+                  <BsFillTelephoneInboundFill />
+                ) : (
+                  <BsFillTelephoneOutboundFill />
+                )}
+                <h1 className="activity_caller">{call.from}</h1>
+              </div>
+              <div className="activity_right">
+                <h2 className="activity_called_at">
+                  {new Date(call.created_at).toLocaleTimeString([], {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })}
+                </h2>
+                <div className="options_icons">
+                  <RiArchiveFill onClick={() => archiveClickHandler(call.id)} />
+                  <RiInformationFill
+                    onClick={() => infoClickHandler(call.id)}
+                  />
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      ))}
+          );
+        })}
     </div>
   );
 }
